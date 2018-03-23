e98bf11791525e1f84bd7876164be28b144e4e33
wangwei <wwwy931201@163.com>
2018-03-23 17:59:35 +0800

    搞事情 Signed-off-by: wangwei <wwwy931201@163.com>

--- src/com/tytt/Demo.java
+++ src/com/tytt/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("test");  
 		System.out.println("master and conflict");
+		System.out.println("master and conflict ¸ãÊÂÇé");
 	}
 	
 	public void git() { 
